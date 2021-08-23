5293d5e028805e2def9e75b4495cb8feb1e8adbe
Tian <yu_tian99@hotmail.com>
2021-08-23 10:40:01 -0400

    add profile training, need refactor

--- tensorflow/sgk/resnet50/profile_layer.py
+++ tensorflow/sgk/resnet50/profile_layer.py
@@ -20,11 +20,11 @@ def load_model_layers(sparse_level):
 	model = resnet50(num_classes=100, sparse_level=sparse_level, save_input_shape=True)
 
 	#load mtx
-	model.build((16,3,32,32))
-	forward_pass(model, np.random.randn(16,3,32,32))
 
 	if sparse_level != 0:
 		restore_from_mtx(model, "./mtx_229_0.1")
+	model.build((16,3,32,32))
+	forward_pass(model, np.random.randn(16,3,32,32))
 	edge_layers = flatten_layers(model)
 	return np.array(list(filter(lambda layer: isinstance(layer, GeneralConv2D), edge_layers)))
 	
@@ -69,30 +69,72 @@ def get_layers_conv3x3():
 		result[i] = (layers_level_0[i], layers_level_1[i], layers_level_3[i])
 	return result, indexes
 
-def profile_particular_layer(layer, inputs, num_iter, logdir):
+def profile_particular_layer_inference(layer, exaust_input, model_input, num_exaust_iter, logdir):
 	graph = tf.function(layer)
-	for _ in range(30):
-		graph(inputs)
+	for _ in range(num_exaust_iter):
+		graph(exaust_input)
+	with tf.profiler.experimental.Profile(logdir):
+		for x in model_input:
+			graph(x)
+
+
+"""
+Just a comment about descriptor bug:
+https://pgaleone.eu/tensorflow/tf.function/2019/03/21/dissecting-tf-function-part-1/
+Great explanation!!
+"""
+def get_steps_function():
+	@tf.function
+	def steps(batch_data, model, opt):
+		"""
+		Helper function for profiling the layer
+		when training. Since the backward is already defined,
+		I'll just use the reduce mean operation for simplicity
+		"""
+		with tf.GradientTape() as tape:
+			loss_value = tf.reduce_mean(model(batch_data))
+		gradients = tape.gradient(loss_value, model.trainable_variables)
+		opt.apply_gradients(zip(gradients, model.trainable_variables))
+	return steps
+
+def profile_particular_layer_train(layer, exaust_input, model_input, num_exaust_iter, logdir):
+	opt = tf.keras.optimizers.SGD(learning_rate=0.01)
+	steps = get_steps_function()
+	for _ in range(num_exaust_iter):
+		steps(exaust_input, layer, opt)	
 
 	with tf.profiler.experimental.Profile(logdir):
-		for _ in range(num_iter):
-			graph(inputs)
+		for x in tqdm(model_input):
+			steps(x, layer, opt)
 
 
-def profile_conv1x1(logdir):
+def profile_conv1x1_inference(logdir):
 	layers, indexes = get_layers_conv1x1()
 	dense, masked_dense, sparse_1x1 = layers[indexes[0]]
-	input_val = get_layer_input(dense)
-	profile_particular_layer(dense, input_val, 20, "./{}/conv1x1/dense".format(logdir))
-	profile_particular_layer(masked_dense, input_val, 20, "./{}/conv1x1/masked_dense".format(logdir))
-	profile_particular_layer(sparse_1x1, input_val, 20, "./{}/conv1x1/sparse_1x1".format(logdir))
+	exaust_input, model_input = get_layer_input(dense, 20)
+	profile_particular_layer_inference(dense, exaust_input, model_input, 20, "./{}/conv1x1/dense".format(logdir))
+	profile_particular_layer_inference(masked_dense, exaust_input, model_input, 20, "./{}/conv1x1/masked_dense".format(logdir))
+	profile_particular_layer_inference(sparse_1x1, exaust_input, model_input, 20, "./{}/conv1x1/sparse_1x1".format(logdir))
 
-	
-def get_layer_input(layer):
-	return np.random.randn(*layer.layer_input_shape)
+def profile_conv1x1_train(logdir):
+	layers, indexes = get_layers_conv1x1()
+	dense, masked_dense, sparse_1x1 = layers[indexes[0]]
+	exaust_input, model_input = get_layer_input(dense, 20)
+	profile_particular_layer_train(dense, exaust_input, model_input, 20, "./{}/conv1x1/dense".format(logdir))
+	profile_particular_layer_train(masked_dense, exaust_input, model_input, 20, "./{}/conv1x1/masked_dense".format(logdir))
+	profile_particular_layer_train(sparse_1x1, exaust_input, model_input, 20, "./{}/conv1x1/sparse_1x1".format(logdir))
+
+
+def get_layer_input(layer, num_iter):
+	input_shape = layer.layer_input_shape
+	exaust_input = np.random.randn(*input_shape)
+	model_input = np.random.randn(input_shape[0] * num_iter, *input_shape[1:])
+	model_input = tf.data.Dataset.from_tensor_slices(model_input)
+	model_input = model_input.batch(input_shape[0])
+	return exaust_input, model_input
 
 if __name__ == "__main__":
-	profile_conv1x1("profiler_logs")
+	profile_conv1x1_train("profiler_logs")
 	# load_model_layers(sparse_level=1)
 
 	# a = resnet50(num_classes=100, sparse_level=0, save_input_shape=True)
